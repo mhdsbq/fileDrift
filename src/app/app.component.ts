ed40b4f75fa815bc5105e4b27182e6c9a961add7
Muhammed Sabique <muhammedsabique@Muhammeds-Air.bbrouter>
2024-07-01 21:31:54 +0530

    Refactor: Moved network initialization to app service from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PeersGalleryComponent } from './components/peers-gallery/peers-gallery.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { SignalRService } from './services/root-services/signalr.service';
-import { Observable, combineLatest, take } from 'rxjs';
+import { Observable } from 'rxjs';
 import { PeerService } from './services/root-services/peer.service';
 import { ConnectionDirectoryService } from './services/root-services/connection-directory.service';
+import { AppService } from './services/app.service';
 
 @Component({
   selector: 'app-root',
@@ -15,62 +15,19 @@ import { ConnectionDirectoryService } from './services/root-services/connection-
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   public connectionId: string;
   public connections$: Observable<string[]>;
 
-  @HostListener('window:beforeunload')
-  leaveRoomBeforeUnload() {
-    this._signalRService.leaveRoom(this.connectionId);
-  }
-
   public constructor(
-    private _signalRService: SignalRService,
+    private _appService: AppService,
     private _peerService: PeerService,
     private _connectionDirectory: ConnectionDirectoryService
   ) {
+    if (!_appService.isInitialized) {
+      console.error('[APP] App service has not been initialized!!!');
+    }
     this.connections$ = this._connectionDirectory.connections$;
     this.connectionId = this._peerService.id;
-  }
-
-  public async ngOnInit() {
-    this._handleConnection();
-    this._handleJoinRoomMessage();
-    this._handleLeaveRoomMessage();
-    this._signalRService.initializeConnection();
-  }
-
-  public ngOnDestroy(): void {
-    this._signalRService.leaveRoom(this.connectionId);
-  }
-
-  private _handleConnection() {
-    combineLatest([this._signalRService.onConnection, this._peerService.onOpen$]).subscribe(([_, isPeerOpen]) => {
-      if (!isPeerOpen) {
-        console.info('[app] waiting for peer to open');
-        return;
-      }
-      this._signalRService.joinRoom(this.connectionId);
-      console.info('[sig] Sent connection request to server.');
-    });
-  }
-
-  private _handleJoinRoomMessage() {
-    this._signalRService.joinRoomMessage.subscribe((joinRoomMessage) => {
-      if (joinRoomMessage.from === this.connectionId) {
-        console.warn('[app] Self connection message was forwarded.');
-        return;
-      }
-
-      this._peerService.connect(joinRoomMessage.from);
-      console.info(`[sig] Connected to ${joinRoomMessage.from}`);
-    });
-  }
-
-  private _handleLeaveRoomMessage() {
-    this._signalRService.leaveRoomMessage.subscribe((leaveRoomMessage) => {
-      this._peerService.disconnect(leaveRoomMessage.from);
-      console.info(`[sig] Disconnected from ${leaveRoomMessage.from}`);
-    });
   }
 }
--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,6 +1,59 @@
-import { Injectable } from '@angular/core';
+import { HostListener, Injectable } from '@angular/core';
+import { PeerService } from './root-services/peer.service';
+import { SignalRService } from './root-services/signalr.service';
+import { combineLatest } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-class AppService {
-  constructor() {}
+export class AppService {
+  @HostListener('window:beforeunload')
+  leaveRoomBeforeUnload() {
+    console.warn('Leaving the room since app has been quit.');
+    this._signalRService.leaveRoom(this.connectionId);
+  }
+
+  private readonly connectionId;
+  public isInitialized: boolean = false;
+
+  public constructor(private _signalRService: SignalRService, private _peerService: PeerService) {
+    this.connectionId = this._peerService.id;
+    this.initialize();
+  }
+
+  public initialize(): void {
+    this._handleConnection();
+    this._handleJoinRoomMessage();
+    this._handleLeaveRoomMessage();
+    this._signalRService.initializeConnection();
+    this.isInitialized = true;
+  }
+
+  private _handleConnection() {
+    combineLatest([this._signalRService.onConnection, this._peerService.onOpen$]).subscribe(([_, isPeerOpen]) => {
+      if (!isPeerOpen) {
+        console.info('[app] waiting for peer to open');
+        return;
+      }
+      this._signalRService.joinRoom(this.connectionId);
+      console.info('[sig] Sent connection request to server.');
+    });
+  }
+
+  private _handleJoinRoomMessage() {
+    this._signalRService.joinRoomMessage.subscribe((joinRoomMessage) => {
+      if (joinRoomMessage.from === this.connectionId) {
+        console.warn('[app] Self connection message was forwarded.');
+        return;
+      }
+
+      this._peerService.connect(joinRoomMessage.from);
+      console.info(`[sig] Connected to ${joinRoomMessage.from}`);
+    });
+  }
+
+  private _handleLeaveRoomMessage() {
+    this._signalRService.leaveRoomMessage.subscribe((leaveRoomMessage) => {
+      this._peerService.disconnect(leaveRoomMessage.from);
+      console.info(`[sig] Disconnected from ${leaveRoomMessage.from}`);
+    });
+  }
 }
